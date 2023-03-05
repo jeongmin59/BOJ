74db1fdcf60014668c3c2c1c45f47ed34ea217c2
jeongmin <jmgong59@gmail.com>
2023-03-06 07:49:32 +0900

    220305

---
+++ IM 대비/10709_기상캐스터.py
@@ -0,0 +1,29 @@
+# 10709_기상캐스터
+'''
+- 입력은 구름이 있는 경우 'c'로 주어지고 없는 경우 '.'으로 주어짐
+- 처음부터 구역에 구름이 있는 경우 0을 출력, 몇 분이 지나도 뜨지 않을 경우 -1을 출력해야 함
+
+1. 빈 리스트 만들기
+2. 입력 받은 리스트를 순회 하면서 다음의 조건에 맞게 값 바꾸기
+- cnt를 -1로 설정
+- 만약 입력 받은 리스트의 값이 c라면 cnt = 0
+- cnt가 0보다 크거나 같을 경우 + 1 아니면 그대로 -1
+- 빈 리스트에 cnt 값으로 대체 하기
+'''
+
+H, W = map(int, input().split())
+c_lst = [list(input()) for _ in range(H)]
+cloud = [[0 for _ in range(W)] for _ in range(H)]
+
+for i in range(H):
+    cnt = -1    # "c"를 만나지 못하면 계속 -1
+    for j in range(W):
+        if c_lst[i][j] == "c":
+            cnt = 0
+        else:
+            if cnt >= 0:
+                cnt += 1
+        cloud[i][j] = cnt
+
+for c in cloud:
+    print(*c)
---
+++ IM 대비/2578_빙고.py
@@ -0,0 +1,37 @@
+# 2578_빙고
+'''
+- 5 * 5 빙고판이며 가로, 세로, 대각선 포함해서 3줄 이상 빙고
+- 빙고판과 숫자를 돌면면서 숫자가 선언되면 빙고판에서 숫자 remove
+- 빙고판이 [] 빈 리스트가 되면 1씩 올려주고 3이 되면 break
+'''
+
+board1 = [list(map(int, input().split())) for _ in range(5)]    # 원래 빙고판 (가로 빙고)
+board2 = list(map(list, zip(*board1)))  # 전치 행렬 (세로 빙고)
+
+board3 = []
+board4 = []
+for i in range(len(board1)):
+    board3.append(board1[i][i])     # 왼쪽 대각선 빙고
+    board4.append(board2[i][4 - i]) # 오른쪽 대각선 빙고
+
+board = board1 + board2
+board.append(board3)
+board.append(board4)
+
+num = []
+for _ in range(5):
+    num += list(map(int, input().split()))
+
+ans = 0
+for i in num:
+    ans += 1
+    bingo = 0
+    for j in board:
+        if i in j:
+            j.remove(i)
+        if j == []:
+            bingo += 1
+    if bingo >= 3:
+        break
+
+print(ans)
